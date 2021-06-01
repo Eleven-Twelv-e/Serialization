6cdb454f5abacbd7d3b7a7c1dd253d42db4ea4b2
Eleven-Twelv-e <nicat.maharramov.2020@gmail.com>
2021-06-01 15:26:33 +0400

    second commit

--- src/Main.java
+++ src/Main.java
@@ -9,29 +9,10 @@ import java.util.zip.ZipOutputStream;
 public class Main  {
     static LocalDateTime time = LocalDateTime.now();
     static StringBuilder sb = new StringBuilder("");
-    static String path = "D:\\Java Web Projects\\Directories\\Games";
+    static String ps = File.separator;
+    static String path = "D:"+ps+"Java Web Projects"+ps+"Directories"+ps+"Games";
 
-   static File file = new File(path);
-   static File file1 = new File(path + "\\src");
-    static File file2 = new File(path + "\\res\\drawables");
-    static File file3 = new File(path + "\\savegames");
-    static File file4 = new File(path + "\\temp");
-    static File file44 = new File(path + "\\temp\\temp.txt");
-    static File file5 = new File(path + "\\src\\main");
-    static File file51 = new File(path + "\\src\\main\\Main.java");
-    static File file55 = new File(path + "\\src\\main\\Utils.java");
-    static File file6 = new File(path + "\\src\\test");
-    static File file7 = new File(path + "s\\res\\vectors");
-    static File file8 = new File(path + "\\res\\icons");
-    static Scanner sc;
 
-    static {
-        try {
-            sc = new Scanner(file44);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
 
     public static void main(String[] args) throws IOException {
 
@@ -44,64 +25,55 @@ public class Main  {
         gameProgressPath.add(path+"\\savegames\\");
         gameProgressPath.add(path+"\\savegames\\");
 
+        createFolders(path+ps+"src"+ps+"main");
+        createFiles(path+ps+"src"+ps+"test");
+        createFolders(path+ps+"res"+ps+"drawables");
+        createFolders(path+ps+"res"+ps+"vectors");
+        createFolders(path+ps+"res"+ps+"icons");
+        createFolders(path+ps+"savegames");
+        createFolders(path+ps+"temp");
 
-        if (file.mkdir() &&
-                file1.mkdirs() &&
-                file2.mkdirs() &&
-                file3.mkdir() &&
-                file4.mkdirs() &&
-                file5.mkdirs() &&
-                file6.mkdir() &&
-                file7.mkdir() &&
-                file8.mkdir()) {
+        createFiles(path+ps+"src"+ps+"main"+ps+"Main.java");
+        createFiles(path+ps+"src"+ps+"main"+ps+"Utils.java");
+        createFiles(path+ps+"temp"+ps+"temp.txt");
 
-            writeLog(sb.append(time + " Directories are create successfully! \n"), sc);
-        } else if (file.exists() &&
-                file1.exists() &&
-                file2.exists() &&
-                file3.exists() &&
-                file4.exists() &&
-                file5.exists() &&
-                file6.exists() &&
-                file7.exists() &&
-                file8.exists()) {
+        saveGame(path+ps+"savegames"+ps+"one.dat",gmOne);
+        saveGame(path+ps+"savegames"+ps+"two.dat",gmTwo);
+        saveGame(path+ps+"savegames"+ps+"three.dat",gmTree);
 
-            writeLog(sb.append(time + " Directories are already exist! \n"), sc);
-        } else {
-
-            writeLog(sb.append(time + " Error when try to create directories! \n"), sc);
-        }
-
-        try {
-            if (file4.createNewFile() &&
-                    file44.createNewFile() &&
-                    file5.createNewFile() &&
-                    file55.createNewFile() &&
-                    file51.createNewFile()) {
-                writeLog(sb.append(time + " Files are created successfully! \n"), sc);
-            } else if (file4.exists() &&
-                    file44.exists() &&
-                    file5.exists() &&
-                    file55.exists() &&
-                    file51.exists()) {
-                writeLog(sb.append(time + " Files are already exist!  \n"), sc);
-            }
-        } catch (IOException e) {
-            writeLog(sb.append(time + " Error when try to create files!\n " + e), sc);
-            e.printStackTrace();
-        }
-
-        saveGame(path+"\\savegames\\",gmOne);
-        saveGame(path+"\\savegames\\",gmTwo);
-        saveGame(path+"\\savegames\\",gmTree);
-
-        zipFiles(path+"\\savegames\\",gameProgressPath);
+        zipFiles(path+ps+"savegames"+ps,gameProgressPath);
+        deleteFiles(path+"");
 
 
     }
 
-    public static void writeLog(StringBuilder sb, Scanner sc) throws IOException{
-        try (FileWriter write = new FileWriter(path + "\\temp\\temp.txt")) {
+    private static File createFiles(String path) throws IOException{
+        File file = new File(path);
+        if (file.createNewFile()){
+            writeLog(sb.append(time + " Files are created successfully! \n"));
+        }else if (file.exists()){
+            writeLog(sb.append(time + " Files are already exist!  \n"));
+        }else {
+            writeLog(sb.append(time + " Error when try to create files!\n " ));
+        }
+        return file;
+    }
+
+
+    private static File createFolders(String path) throws IOException {
+        File file = new File(path);
+        if(file.mkdirs()){
+            writeLog(sb.append(time + " Directories are create successfully! \n"));
+        }else if (file.exists()){
+            writeLog(sb.append(time + " Directories are already exist! \n"));
+        }else {
+            writeLog(sb.append(time + " Error when try to create directories! \n"));
+        }
+        return file;
+    }
+
+    public static void writeLog(StringBuilder sb) throws IOException{
+        try (FileWriter write = new FileWriter(path + ps+"temp"+ps+"temp.txt")) {
             write.write("\n" + sb.append("\n"));
 
         } catch (IOException e) {
@@ -153,7 +125,7 @@ public class Main  {
             }
         } else {
             try {
-                writeLog(sb.append(time + " Nothing to delete \n"), sc);
+                writeLog(sb.append(time + " Nothing to delete \n"));
 
             } catch (IOException e) {
                 e.printStackTrace();
